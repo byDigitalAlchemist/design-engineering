4bc2692900b5665a9afae2b3d3ac71422bc1a502
Digital Alchemist <developer.anwar@gmail.com>
2017-07-27 22:35:28 -0400

    Signed-off-by: Digital Alchemist <developer.anwar@gmail.com>

---
+++ design-patterns/src/main/java/com/dc/dp/solutions/visitor/Consumer.java
@@ -0,0 +1,15 @@
+package com.dc.dp.solutions.visitor;
+
+public class Consumer implements IVisitor {
+	@Override
+	public void visit(Phone phone) {
+	}
+
+	@Override
+	public void visit(Charger charger) {
+	}
+
+	@Override
+	public void visit(Handfree handfree) {
+	}
+}
